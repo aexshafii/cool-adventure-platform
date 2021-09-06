d23f45f37d7f377a78627d341f21e166ae4a8041
Alexandre Shafii <alex@prologe.io>
2021-09-06 14:49:29 -0700

    feat: responsive navbar v2

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import logo from "../assets/logo.svg"
-import { FaAlignJustify } from "react-icons/fa"
+import { FiAlignJustify } from "react-icons/fi"
 const Navbar = () => {
   return (
     <nav className="navbar">
@@ -10,35 +10,32 @@ const Navbar = () => {
           <Link to="/">
             <img src={logo} alt="brackets-logo" />
           </Link>
-          <button className="nav-btn">
-            <FaAlignJustify />
+          <button>
+            <FiAlignJustify />
           </button>
         </div>
-        <div className="nav-links show-links">
-          <div className="nav-link contact-link">
+
+        <div className="nav-link contact-link">
+          <div className={"nav-links show-links"}>
             <Link
-              to="/whats-included"
+              to="/recipes"
               className="nav-link"
               activeClassName="active-link"
             >
-              What's included?
+              What's Included
             </Link>
-            <Link
-              to="/pricing"
-              className="nav-link"
-              activeClassName="active-link"
-            >
-              Pricing
-            </Link>
-            <Link
-              to="/signin"
-              className="nav-link"
-              activeClassName="active-link"
-            >
+            <Link to="/tags" className="nav-link" activeClassName="active-link">
               Sign in
             </Link>
-            <Link to="/getstarted" className="nav-link" className="btn">
-              Get started
+            <Link
+              to="/about"
+              className="nav-link"
+              activeClassName="active-link"
+            >
+              Pricing{" "}
+            </Link>
+            <Link to="/contact" className="btn">
+              Get Started{" "}
             </Link>
           </div>
         </div>
