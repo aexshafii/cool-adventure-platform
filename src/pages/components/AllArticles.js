539db3ccc58064d37d415d5b7311131de36935f8
Alexandre Shafii <alex@prologe.io>
2021-09-07 00:23:33 -0700

    feat: filter query for English only

--- src/pages/components/AllArticles.js
+++ src/pages/components/AllArticles.js
@@ -1,6 +1,27 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+
+const query = graphql`
+  {
+    allContentfulArticle(filter: { node_locale: { eq: "en-US" } }) {
+      nodes {
+        richText {
+          raw
+        }
+        title
+        node_locale
+        image {
+          gatsbyImageData
+        }
+      }
+    }
+  }
+`
 
 const AllArticles = () => {
+  const data = useStaticQuery(query)
+  const articles = data.allContentfulArticle.nodes
+  console.log(articles)
   return (
     <div>
       <h4>All Articles</h4>
