5ee1ed799df800ff735e84de22e8b97d5ada29da
Alexandre Shafii <alex@prologe.io>
2021-09-06 18:24:46 -0700

    feat: toggle nav links functional button

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -1,8 +1,10 @@
 import React from "react"
+import { useState } from "react"
 import { Link } from "gatsby"
 import logo from "../assets/logo.svg"
 import { FiAlignJustify } from "react-icons/fi"
 const Navbar = () => {
+  const [show, setShow] = useState(false)
   return (
     <nav className="navbar">
       <div className="nav-center">
@@ -10,13 +12,12 @@ const Navbar = () => {
           <Link to="/">
             <img src={logo} alt="brackets-logo" />
           </Link>
-          <button className="nav-btn">
+          <button className="nav-btn" onClick={() => setShow(!show)}>
             <FiAlignJustify />
           </button>
         </div>
-
-        <div className=" nav-links-container">
-          <div className={"nav-links show-links"}>
+        <div className="nav-links-container">
+          <div className={show ? "nav-links show-links" : "nav-links"}>
             <Link
               to="/whatsincluded"
               className="nav-link"
